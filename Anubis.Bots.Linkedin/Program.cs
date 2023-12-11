38214d6cb15c93bef9cbe7b3258ea025f82d04b1
Netanel Abergel <89765095+netanel-abergel@users.noreply.github.com>
2023-12-11 22:46:44 +0200

    refactoring

--- Anubis.Bots.Linkedin/Program.cs
+++ Anubis.Bots.Linkedin/Program.cs
@@ -20,58 +20,80 @@ namespace Anubis.Bots.Linkedin
             // Initialize the Chrome WebDriver
             IWebDriver driver = new ChromeDriver();
 
-            // Open the webpage
-            driver.Navigate().GoToUrl(startUrl);
-            
-            IWebElement login_UserName = driver.FindElement(By.Id("username"));
-            
-            login_UserName.SendKeys(userName);
-            
-            IWebElement login_Pass = driver.FindElement(By.Id("password"));
-            
-            login_Pass.SendKeys(password);
-            
-            //aria-label="Sign in"
-            IWebElement login_Button = driver.FindElement(By.CssSelector("[aria-label='Sign in']"));
-            
-            login_Button.Click();
+            Login(driver);
 
-            // var tmpSearchPage = searchPage.Replace("#{searchCriteria}", searchCriteria);
-            // driver.Navigate().GoToUrl(tmpSearchPage);
-            //IWebElement messageButton = driver.FindElement(By.CssSelector("[aria-label='Message ']"));
+            VisitUserProfile(driver);
+            
+            IsUserInvitePending(driver);
+            
+            SendConnectInvite(driver);
 
-            // open user profile by id
-            var userId = "assaf-atias-84099832";
-            
-            var userUrl = $"https://www.linkedin.com/in/{userId}/";
-            driver.Navigate().GoToUrl(userUrl);
-            
+            SendMessageToUser(driver);
+
+            // Close the browser after task completion
+            driver.Quit();
+        }
+
+        private static void SendMessageToUser(IWebDriver driver)
+        {
+            // send message to the user
+            var cssSelector = "[aria-label*='Message ']";
+            IWebElement messageButton = driver.FindElement(By.CssSelector(cssSelector));
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", messageButton);
+
+            Thread.Sleep(500);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", messageButton);
+
             Thread.Sleep(1000);
-            
-            // check if the user is already connected
-            var pendingBtnExists = driver.FindElement(By.CssSelector("[aria-label*='Pending, ']"));
-            if (pendingBtnExists != null) // user is waiting for connection approval 
-            {
-                return;
-            }
-            
+
+            // send message
+            IWebElement messageTextArea = driver.FindElement(By.CssSelector("[aria-label='Write a message…']"));
+
+            messageTextArea.SendKeys("Hi, my name is Netanel. Let's connect!");
+
+            IWebElement buttonSendMessage =
+                driver.FindElements(By.CssSelector(".artdeco-button"))?.FirstOrDefault(x =>
+                    x.Text.StartsWith("Send", StringComparison.InvariantCultureIgnoreCase));
+
+            buttonSendMessage.Click();
+
+            // wait until the click ^ action will be done!
+            Thread.Sleep(1000);
+
+            // close the message box
+            IWebElement buttonCloseConversation = driver.FindElements(By.CssSelector(".artdeco-button__text"))
+                ?.FirstOrDefault(x =>
+                    x.Text.StartsWith("Close your conversation", StringComparison.InvariantCultureIgnoreCase));
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonCloseConversation);
+        }
+
+        private static bool SendConnectInvite(IWebDriver driver)
+        {
             // connect
             IWebElement buttonConnect =
                 driver.FindElement(By.CssSelector("[aria-label*='Invite']"));
-            
-            Thread.Sleep(500);
-            
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonConnect);
-            
-            //Invitation not sent to the user. You can resend an invitation 3 weeks after withdrawing it.
-            IWebElement withdrawingStatus =
-                driver.FindElement(By.CssSelector(".artdeco-toast-item__message"));
 
-            if (withdrawingStatus != null)
+            Thread.Sleep(500);
+
+            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonConnect);
+
+            try
             {
-                return;
+                //Invitation not sent to the user. You can resend an invitation 3 weeks after withdrawing it.
+                IWebElement withdrawingStatus =
+                    driver.FindElement(By.CssSelector(".artdeco-toast-item__message"));
+
+                if (withdrawingStatus != null)
+                {
+                    return false;
+                }
             }
-      
+            catch (Exception e)
+            {  }
+            
             // add a note
             IWebElement buttonAddANote =
                 driver.FindElement(By.CssSelector("[aria-label='Add a note']"));
@@ -94,41 +116,64 @@ namespace Anubis.Bots.Linkedin
             
             buttonSendNote.Click();
             
-            // send message to the user
-            var cssSelector = "[aria-label*='Message ']";
-            IWebElement messageButton = driver.FindElement(By.CssSelector(cssSelector));
+            return true; 
+        }
 
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", messageButton);
+        private static bool IsUserInvitePending(IWebDriver driver)
+        {
+            try
+            {
+                // check if the user is already connected
+                var pendingBtnExists = driver.FindElement(By.CssSelector("[aria-label*='Pending, ']"));
+                if (pendingBtnExists != null) // user is waiting for connection approval 
+                {
+                    return true;
+                }
+            }
+            catch (Exception e)
+            { }
+
+            return false;
+        }
+
+        private static void Login(IWebDriver driver)
+        {
+            // Open the webpage
+            driver.Navigate().GoToUrl(startUrl);
             
-            Thread.Sleep(500);
+            IWebElement login_UserName = driver.FindElement(By.Id("username"));
             
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", messageButton);
+            login_UserName.SendKeys(userName);
+            
+            IWebElement login_Pass = driver.FindElement(By.Id("password"));
+            
+            login_Pass.SendKeys(password);
+            
+            //aria-label="Sign in"
+            IWebElement login_Button = driver.FindElement(By.CssSelector("[aria-label='Sign in']"));
+            
+            login_Button.Click();
+        }
+
+        private static void VisitUserProfile(IWebDriver driver)
+        {
+            // open user profile by id
+            var userId = "assaf-atias-84099832";
+            
+            var userUrl = $"https://www.linkedin.com/in/{userId}/";
+            driver.Navigate().GoToUrl(userUrl);
             
             Thread.Sleep(1000);
-            
-            // send message
-            IWebElement messageTextArea = driver.FindElement(By.CssSelector("[aria-label='Write a message…']"));
-            
-            messageTextArea.SendKeys("Hi, my name is Netanel. Let's connect!");
-            
-            IWebElement buttonSendMessage =
-                driver.FindElements(By.CssSelector(".artdeco-button"))?.FirstOrDefault(x =>
-                    x.Text.StartsWith("Send", StringComparison.InvariantCultureIgnoreCase));
-            
-            buttonSendMessage.Click();
-            
-            // wait until the click ^ action will be done!
-            Thread.Sleep(1000);
-            
-            // close the message box
-            IWebElement buttonCloseConversation = driver.FindElements(By.CssSelector(".artdeco-button__text"))
-                ?.FirstOrDefault(x =>
-                    x.Text.StartsWith("Close your conversation", StringComparison.InvariantCultureIgnoreCase));
-            
-            ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonCloseConversation);
-            
-            // Close the browser after task completion
-            driver.Quit();
+        }
+
+        private static void SendConnectionRequest(IWebDriver driver)
+        {
+            // Code for sending a connection request goes here
+        }
+
+        private static void AddNoteAndSendMessage(IWebDriver driver)
+        {
+            // Code for adding a note and sending a message goes here
         }
     }
 }
