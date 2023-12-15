03e41e1e5a27c053dae04d2696745771e3736e31
Netanel Abergel <89765095+netanel-abergel@users.noreply.github.com>
2023-12-15 14:15:41 +0200

    fixes

--- Anubis.Bots.Linkedin/Program.cs
+++ Anubis.Bots.Linkedin/Program.cs
@@ -5,6 +5,7 @@ using System.Threading;
 using Newtonsoft.Json;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using Cookie = System.Net.Cookie;
 
 namespace Anubis.Bots.Linkedin
@@ -21,36 +22,16 @@ namespace Anubis.Bots.Linkedin
             
             driver.Manage().Window.Maximize();
 
-            var feedUrl = "https://www.linkedin.com/feed/";
+            // var userName = "neta931test931@gmail.com"; //"neta931test@gmail.com";
+            // var password = "bcfhF.xe28!jdY8";
+            // Login(driver, userName, password);
             
-            // check if user is logged in
-            driver.Navigate().GoToUrl(feedUrl);
-            Thread.Sleep(2000);
-            var cookies = driver.Manage().Cookies;
-            var cookie = cookies.GetCookieNamed("li_at");
-            if (cookie == null || driver.Url != feedUrl)
-            {
-                SetCookiesFromFile(driver);
-                
-                // check if user is logged in
-                driver.Navigate().GoToUrl("https://www.linkedin.com/feed/");
-                
-                cookie = cookies.GetCookieNamed("li_at");
-                
-                if (cookie == null|| driver.Url != feedUrl)
-                {
-                    // login
-                    string userName = "neta931test931@gmail.com"; //"neta931test@gmail.com";
-                    string password = "bcfhF.xe28!jdY8";
-                    string loginPageUrl = "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fsearch%2Fresults%2Fall%2F%3Fkeywords%3D"+searchCriteria+"&fromSignIn=true&trk=cold_join_sign_in";
-                    Login(driver, loginPageUrl, userName, password);
-                }
-            }
-            
+            var cookies = ImportCookiesFromFile("cookies.txt");
+            Login(driver, cookies);
 
             driver.Navigate().GoToUrl(searchPage.Replace("#{searchCriteria}", searchCriteria));
             
-            KeepCookiesInFile(driver);
+            ExportPageCookiesToFile(driver, "cookies.txt");
             
             DummyMouseMovement(driver);
             
@@ -74,33 +55,109 @@ namespace Anubis.Bots.Linkedin
             else
                 SendMessageToUser(driver, userId);
 
-            KeepCookiesInFile(driver);
-
+            ExportPageCookiesToFile(driver, "cookies.txt");
+            
+            VisitPage(driver, "https://www.linkedin.com/feed/");
+            
+            var startPostButton = WaitUntilLoaded(driver, ".share-box-feed-entry__trigger");
+            
+            startPostButton.Click();
+            
+            var postTextArea = WaitUntilLoaded(driver, "[aria-label='Text editor for creating content']");
+            
+            postTextArea.SendKeys("testtttt");
+            
+            var postButton = WaitUntilLoaded(driver, ".share-actions__primary-action");
+            
+            postButton.Click();
+            
             // Close the browser after task completion
             driver.Quit();
         }
 
-        private static void SetCookiesFromFile(IWebDriver driver, string fileName ="cookies.txt")
+        private static void EnsureLoggedIn(IWebDriver driver, Cookie[] cookies)
         {
-            var cookiesStr = File.ReadAllText(fileName);
-            var cookies = JsonConvert.DeserializeObject<Cookie[]>(cookiesStr);
+            if (IsUserLoggedIn(driver)) return;
+            
+            Login(driver, cookies);
+        }
+        
+        private static void EnsureLoggedIn(IWebDriver driver, string userName, string password, bool forceLogin = false)
+        {
+            if (IsUserLoggedIn(driver) && !forceLogin) return;
 
+            
+            
+            // login
+          
+            // "https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fsearch%2Fresults%2Fall%2F%3Fkeywords%3D" +
+                // searchCriteria + "&fromSignIn=true&trk=cold_join_sign_in";
+            
+            Login(driver, userName, password);
+        }
+
+        private static bool IsUserLoggedIn(IWebDriver driver)
+        {
+            var feedUrl = "https://www.linkedin.com/feed/";
+            VisitPage(driver, feedUrl);
+
+            if (driver.Url == feedUrl)
+            {
+                WaitUntilLoaded(driver, ".share-box-feed-entry__top-bar");
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void VisitPage(IWebDriver driver, string pageUrl)
+        {
+            if(driver.Url != pageUrl)
+                driver.Navigate().GoToUrl(pageUrl);
+        }
+
+        private static void Login(IWebDriver driver, Cookie[] cookies)
+        {
+            VisitPage(driver, "https://www.linkedin.com/feed/");
+            
             foreach (var cookie in cookies)
             {
                 driver.Manage().Cookies.AddCookie(new OpenQA.Selenium.Cookie(cookie.Name, cookie.Value, cookie.Domain,
                     cookie.Path, DateTime.Now.AddDays(365), cookie.Secure, cookie.HttpOnly, "None"));
             }
+
+            if (!IsUserLoggedIn(driver)) throw new UnauthorizedAccessException("Failed to login");
+        }
+        
+        private static void Login(IWebDriver driver, string cookiesSourceFilePath = "cookies.txt")
+        {
+            var cookies = ImportCookiesFromFile(cookiesSourceFilePath);
+
+            Login(driver, cookies);
         }
 
-        private static void KeepCookiesInFile(IWebDriver driver, string fileName ="cookies.txt")
+        private static Cookie[] ImportCookiesFromFile(string cookiesSourceFilePath)
         {
-            var cookies = driver.Manage().Cookies.AllCookies.ToArray();
-            using (var f = File.Create(fileName))
+            var cookiesStr = File.ReadAllText(cookiesSourceFilePath);
+            var cookies = JsonConvert.DeserializeObject<Cookie[]>(cookiesStr);
+            return cookies;
+        }
+
+        private static void ExportPageCookiesToFile(IWebDriver driver, string cookiesTargetFilePath = "cookies.txt")
+        {
+            var cookies = GetPageCookies(driver);
+            using (var f = File.Create(cookiesTargetFilePath))
             {
                 var cookiesStr = JsonConvert.SerializeObject(cookies);
                 f.Write(System.Text.Encoding.UTF8.GetBytes(cookiesStr), 0, cookiesStr.Length);
                 f.Close();
             }
+        }
+
+        private static OpenQA.Selenium.Cookie[] GetPageCookies(IWebDriver driver)
+        {
+            var cookies = driver.Manage().Cookies.AllCookies.ToArray();
+            return cookies;
         }
 
         private static void DummyMouseMovement(IWebDriver driver)
@@ -131,7 +188,7 @@ namespace Anubis.Bots.Linkedin
             // Wait for a moment to see the scroll effect (you can adjust the duration)
             Thread.Sleep(1000);
             
-            // Thread.Sleep(rnd.Next(500, 5000)); // Sleep for a random amount of time (1-5 seconds) to simulate a human
+            // Thread.Sleep(rnd.Next(500, 5000)); // Sleep for a random amount of time (1-5 waitSeconds) to simulate a human
             
             // Perform dummy mouse movements
             // You can chain multiple actions together
@@ -154,17 +211,17 @@ namespace Anubis.Bots.Linkedin
             // Wait for a moment to see the scroll effect (you can adjust the duration)
             Thread.Sleep(1000);
         }
-        
 
         private static int GetUnseenMessages(IWebDriver driver)
         {
             var unseenMessagesCount = 0;
+            
             try
             {
-                var str = driver.FindElement(By.CssSelector("[title*='unseen message']"))
-                    ?.Text;
+                var element = WaitUntilLoaded(driver, "[title*='unseen message']");
+                var str = element?.Text?.Trim();
                 
-                int.TryParse(str?.Trim(), out unseenMessagesCount);
+                int.TryParse(str, out unseenMessagesCount);
             }
             catch (Exception)
             { }
@@ -191,14 +248,14 @@ namespace Anubis.Bots.Linkedin
             return isConnectedUser;
         }
 
-        private static void SendMessageToUser(IWebDriver driver, string userId, string message = "Hi, my name is Netanel. Let's connect!")
+        private static void SendMessageToUser(IWebDriver driver, 
+            string userId, 
+            string message = "Hi, my name is Netanel. Let's connect!")
         {
             VisitUserProfile(driver, userId);
             
-            // send message to the user
-            var cssSelector = "[aria-label*='Message ']";
-            IWebElement messageButton = driver.FindElement(By.CssSelector(cssSelector));
-
+            var messageButton = WaitUntilLoaded(driver, cssSelector: "[aria-label*='Message ']", waitSeconds:10);
+            
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", messageButton);
 
             Thread.Sleep(500);
@@ -207,26 +264,55 @@ namespace Anubis.Bots.Linkedin
 
             Thread.Sleep(1000);
 
+            var messageTextArea = WaitUntilLoaded(driver, cssSelector: "[aria-label='Write a message…']", waitSeconds:10);
+            
             // send message
-            IWebElement messageTextArea = driver.FindElement(By.CssSelector("[aria-label='Write a message…']"));
+            // var messageTextArea = driver.FindElement(By.CssSelector("[aria-label='Write a message…']"));
 
             messageTextArea.SendKeys(message);
-
-            IWebElement buttonSendMessage =
+            
+            var buttonSendMessage =
                 driver.FindElements(By.CssSelector(".artdeco-button"))?.FirstOrDefault(x =>
                     x.Text.StartsWith("Send", StringComparison.InvariantCultureIgnoreCase));
 
             buttonSendMessage.Click();
 
             // wait until the click ^ action will be done!
-            Thread.Sleep(1000);
+            WaitUntilLoaded(driver, 
+                cssSelector: "[aria-label='Write a message…']", 
+                waitSeconds:10);
 
             // close the message box
-            IWebElement buttonCloseConversation = driver.FindElements(By.CssSelector(".artdeco-button__text"))
+            var buttonCloseConversation = driver.FindElements(By.CssSelector(".artdeco-button__text"))
                 ?.FirstOrDefault(x =>
                     x.Text.StartsWith("Close your conversation", StringComparison.InvariantCultureIgnoreCase));
 
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonCloseConversation);
+        }
+
+        private static IWebElement WaitUntilLoaded(ISearchContext driver, string cssSelector, uint waitSeconds = 10)
+        {
+            DateTime start = DateTime.Now; 
+            IWebElement element = null;
+            var maxIdx = 1000;
+            while (waitSeconds > (DateTime.Now - start).TotalSeconds && maxIdx-- > 0)
+            {
+                try
+                {
+                    element = driver.FindElement(By.CssSelector(cssSelector));
+                    if (element != null)
+                    {
+                        break;
+                    }
+
+                    Thread.Sleep(500);
+                }
+                catch (Exception e)
+                {
+                }
+            }
+            
+            return element ?? throw new ElementNotSelectableException("Element not found");
         }
 
         private static bool SendUserInvite(IWebDriver driver, string userId)
@@ -236,7 +322,7 @@ namespace Anubis.Bots.Linkedin
             Thread.Sleep(1000);
             
             // connect
-            IWebElement buttonConnect =
+            var buttonConnect =
                 driver.FindElement(By.CssSelector("[aria-label*='Invite']"));
 
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", buttonConnect);
@@ -244,7 +330,7 @@ namespace Anubis.Bots.Linkedin
             try
             {
                 //Invitation not sent to the user. You can resend an invitation 3 weeks after withdrawing it.
-                IWebElement withdrawingStatus =
+                var withdrawingStatus =
                     driver.FindElement(By.CssSelector(".artdeco-toast-item__message"));
 
                 if (withdrawingStatus != null)
@@ -256,7 +342,7 @@ namespace Anubis.Bots.Linkedin
             {  }
             
             // add a note
-            IWebElement buttonAddANote =
+            var buttonAddANote =
                 driver.FindElement(By.CssSelector("[aria-label='Add a note']"));
             
             ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].scrollIntoView(true);", buttonAddANote);
@@ -268,12 +354,12 @@ namespace Anubis.Bots.Linkedin
             Thread.Sleep(1000);
             
             // write and send a note with the connection request 
-            IWebElement textAreaNote =
+            var textAreaNote =
                 driver.FindElement(By.CssSelector("[name='message']"));
             
             textAreaNote.SendKeys("Let's connect! Netanel");
             
-            IWebElement buttonSendNote =
+            var buttonSendNote =
                 driver.FindElements(By.CssSelector(".artdeco-button"))?.FirstOrDefault(x =>
                     x.Text.StartsWith("Send", StringComparison.InvariantCultureIgnoreCase));
             
@@ -301,23 +387,27 @@ namespace Anubis.Bots.Linkedin
             return false;
         }
 
-        private static void Login(IWebDriver driver, string loginPageUrl, string userName, string password)
+        private static void Login(IWebDriver driver, string userName, string password)
         {
-            // Open the webpage
-            driver.Navigate().GoToUrl(loginPageUrl);
-            
-            IWebElement login_UserName = driver.FindElement(By.Id("username"));
+            var loginPageUrl =
+                "https://www.linkedin.com/signup/cold-join?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Ffeed%2F&trk=login_reg_redirect";
+
+            VisitPage(driver, loginPageUrl);
+
+            var login_UserName = driver.FindElement(By.Id("username"));
             
             login_UserName.SendKeys(userName);
             
-            IWebElement login_Pass = driver.FindElement(By.Id("password"));
+            var login_Pass = driver.FindElement(By.Id("password"));
             
             login_Pass.SendKeys(password);
             
             //aria-label="Sign in"
-            IWebElement login_Button = driver.FindElement(By.CssSelector("[aria-label='Sign in']"));
+            var login_Button = driver.FindElement(By.CssSelector("[aria-label='Sign in']"));
             
             login_Button.Click();
+            
+            if (!IsUserLoggedIn(driver)) throw new UnauthorizedAccessException("Failed to login");
         }
 
         private static void VisitUserProfile(IWebDriver driver, string userId)
